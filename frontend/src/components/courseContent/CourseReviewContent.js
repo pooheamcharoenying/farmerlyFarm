6d0d2a6549b5f08426e6ba69b6598fdfed71fe78
warpabit <warpabit@gmail.com>
2020-03-02 23:30:45 +0700

    add rating

--- frontend/src/components/courseContent/CourseReviewContent.js
+++ frontend/src/components/courseContent/CourseReviewContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Rate,Progress } from 'antd';
+import { Rate,Progress,Input } from 'antd';
+const { TextArea } = Input;
 export default function CourseReviewContent() {
     return (
         <div className=" w-full flex flex-col items-center py-4 justify-start">
@@ -7,36 +8,63 @@ export default function CourseReviewContent() {
           Rating & Review
         </div>
         <div className="flex flex-col text-center mb-6 md:mb-8 w-8/12">
-        <div className="text-left text-2xl font-semibold">Student feedback</div>
-        <div className="flex w-full">
-        <div className="  flex flex-col" style={{width:"300px"}}>
+        <div className="text-left text-2xl font-semibold flex "><div className="mr-2">Your Rating : </div><Rate allowHalf defaultValue={3.5}/></div>
+        <div className="text-left text-2xl font-semibold flex mt-4"><div className="mr-2" style={{minWidth:"200px"}}>Your Review : </div><TextArea  placeholder="Typing Your Review Here"
+          autoSize={{ minRows: 2, maxRows: 6 }} /> <button
+          className="text-white bg-green-500 p-2 border-2 rounded border-green-600 hover:text-green-600 hover:bg-white ml-2 text-center flex justify-center items-center"
+          style={{width:"80px"}}>
+        SAVE</button></div>
+        <div className="text-left text-2xl font-semibold mt-6">Student Feedback</div>
+        <div className="flex w-full ">
+        <div className="flex flex-col" style={{width:"300px"}}>
             <div className="text-6xl font-bold">4.3</div><Rate allowHalf defaultValue={4.3}/><div>Course Rating</div></div>
         <div className="flex-1  flex flex-col">
             <div className="flex">
             <Progress percent={46} strokeColor={"gray"}  className="flex-1"/>
-            <Rate allowHalf defaultValue={5} className="flex-1"/>
+            <Rate disabled defaultValue={5} className="flex-1"/>
             </div>
             <div className="flex">
             <Progress percent={40} strokeColor={"gray"}  className="flex-1"/>
-            <Rate allowHalf defaultValue={4} className="flex-1"/>
+            <Rate disabled defaultValue={4} className="flex-1"/>
             </div>
             <div className="flex">
             <Progress percent={12} strokeColor={"gray"}  className="flex-1"/>
-            <Rate allowHalf defaultValue={3} className="flex-1"/>
+            <Rate disabled defaultValue={3} className="flex-1"/>
             </div>
             <div className="flex">
             <Progress percent={8} strokeColor={"gray"}  className="flex-1"/>
-            <Rate allowHalf defaultValue={2} className="flex-1"/>
+            <Rate disabled defaultValue={2} className="flex-1"/>
             </div>
             <div className="flex">
             <Progress percent={4} strokeColor={"gray"}  className="flex-1"/>
-            <Rate allowHalf defaultValue={1} className="flex-1"/>
+            <Rate disabled defaultValue={1} className="flex-1"/>
             </div>
     
         </div>
-        <div></div>
+       
         </div>
-          
+        <div className="text-left text-2xl font-semibold mt-6">Reviews</div>
+        <div className="flex flex-col items-center">
+
+        <div id="comment-1" className="flex flex-col w-8/12 bg-white items-start p-2 mt-4 rounded-lg">
+        <div className="flex"><div className="font-medium mr-2">Mustafa Tunc</div>  <Rate disabled defaultValue={1} className="flex-1"/></div>
+        <div className="text-left mt-2">The course was much better than most and gives so good practical ideas.
+Besides, it was repetition in so many lectures therefore, it comes some boring in some lectures. I think it could be shorter.</div>
+        </div>
+
+        <div id="comment-1" className="flex flex-col w-8/12 bg-white items-start p-2 mt-4">
+        <div className="flex"><div className="font-medium mr-2">Mustafa Tunc</div>  <Rate disabled defaultValue={1} className="flex-1"/></div>
+        <div className="text-left mt-2">The course was much better than most and gives so good practical ideas.
+Besides, it was repetition in so many lectures therefore, it comes some boring in some lectures. I think it could be shorter.</div>
+        </div>
+
+        <div id="comment-1" className="flex flex-col w-8/12 bg-white items-start p-2 mt-4">
+        <div className="flex"><div className="font-medium mr-2">Mustafa Tunc</div>  <Rate disabled defaultValue={1} className="flex-1"/></div>
+        <div className="text-left mt-2">The course was much better than most and gives so good practical ideas.
+Besides, it was repetition in so many lectures therefore, it comes some boring in some lectures. I think it could be shorter.</div>
+        </div>
+
+        </div>
         </div>
     
       
