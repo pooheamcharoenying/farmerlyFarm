4baec6b2bb6dc6f8d5c247588a75eb0674a242b7
warpgap <warpgap@gmail.com>
2020-03-12 10:23:34 +0700

    add lession preview

--- frontend/src/components/drag/SubDragCourse.js
+++ frontend/src/components/drag/SubDragCourse.js
@@ -205,7 +205,7 @@ if(itemsPool[0]){
 
                                 <div className="ml-4"> <FaLock
                                   style={{
-                                    color: "#a45503",
+                                    color: "#778899",
                                     display: !item.preview
                                       ? "inline-block"
                                       : "none"
@@ -214,7 +214,7 @@ if(itemsPool[0]){
 
                                 <div className="ml-4"> <FaCheckCircle
                                   style={{
-                                    color: "#778899",
+                                    color: "#96C221",
                                     display: getIsLessionVisitedFn(item)
                                       ? "inline-block"
                                       : "none"
