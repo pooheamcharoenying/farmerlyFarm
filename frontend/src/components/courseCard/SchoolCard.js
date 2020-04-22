bf392ee4f3139e5d023de2a7299df14b1105bed8
Pooh <pooh.e@projectlab.co.th>
2020-04-22 13:42:17 +0700

    MVP phase 1.0 complete

--- frontend/src/components/courseCard/SchoolCard.js
+++ frontend/src/components/courseCard/SchoolCard.js
@@ -6,10 +6,13 @@ export default function SchoolCard({schoolData}) {
         <div style={{minWidth:"200px",maxWidth:"200px",maxHeight:"360px",minHeight:"360px"}} className="bg-white flex flex-col shadow-lg rounded-lg relative ">
         <div className="bg-gray-500 absolute inset-0 opacity-0 hover:opacity-25 cursor-pointer"></div>
         <img className="w-full object-cover rounded-lg rounded-b-none" alt={schoolDataLocal.schoolName} style={{minHeight:"260px",maxHeight:"260px"}} src={schoolDataLocal.schoolImage || "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png"}/>
-        <div className="text-center text-3xl mt-2 font-bold text-gray-900 px-2 capitalize text2line flex justify-center items-center" style={{minHeight:"60px",maxHeight:"60px"}}>
-           {schoolDataLocal.schoolName || ""}
-          
+        <div style={{height:"10px"}}></div>
+        <div className="text-center text-2xl mt-2 font-bold text-gray-900 px-2 capitalize text2line flex justify-center items-center" style={{minHeight:"60px",maxHeight:"60px"}}>
+           {schoolDataLocal.schoolName + " School" || ""}
         </div>
+        {/* <div className="text-center text-2xl mt-2 font-bold text-gray-900 px-2 capitalize text2line flex justify-center items-center" style={{minHeight:"60px",maxHeight:"60px"}}>
+           School
+        </div> */}
         
         
     </div>
