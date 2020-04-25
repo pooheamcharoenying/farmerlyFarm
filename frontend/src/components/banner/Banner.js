be7267f1af4f87067f00a7e179648d9b138b7b7b
Pooh <pooh.e@projectlab.co.th>
2020-04-25 09:21:35 +0700

    MVP phase 1.0 complete

--- frontend/src/components/banner/Banner.js
+++ frontend/src/components/banner/Banner.js
@@ -26,7 +26,7 @@ export default function Banner() {
         style={{ maxWidth: "300px" }}
       >
         Build skills with courses, certificates, and degrees online from
-        brilliant teachers all around the country.
+        brilliant teachers all around Thailand.
       </div>
 
       {!GlobalHook.getGlobalToken && (
